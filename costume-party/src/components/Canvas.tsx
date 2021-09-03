c8ecd0eec0ec568edf4d618676fc3aefde99d1d4
xmliszt <xmliszt@gmail.com>
2021-09-04 00:19:48 +0800

    feat: board game boundary setup

--- costume-party/src/components/Canvas.tsx
+++ costume-party/src/components/Canvas.tsx
@@ -1,18 +1,48 @@
-import React, { useEffect } from "react";
-import { useRef } from "react";
-import "Canvas.css";
+import React, { useEffect, useRef } from "react";
+import "./Canvas.css";
 
 export default function Canvas(): React.ReactElement {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    if (canvas) {
-      const context = canvas.getContext("2d");
-      if (context) {
-        context.fillStyle = "#000000";
-        context.fillRect(0, 0, 100, 300);
-      }
+    const context = canvasRef.current?.getContext("2d");
+    if (context) {
+      context.strokeStyle = "#515151";
+      context.lineWidth = 10;
+      // Boundary wall
+      context.strokeRect(
+        0,
+        0,
+        canvas?.clientWidth || 600,
+        canvas?.clientHeight || 600
+      );
+      // Center room wall
+      context.strokeRect(200, 200, 200, 200);
+      // Draw north wall
+      context.beginPath();
+      context.moveTo(300, 0);
+      context.lineTo(300, 200);
+      context.stroke();
+      context.closePath();
+      // Draw west wall
+      context.beginPath();
+      context.moveTo(0, 300);
+      context.lineTo(200, 300);
+      context.stroke();
+      context.closePath();
+      // Draw south wall
+      context.beginPath();
+      context.moveTo(300, 600);
+      context.lineTo(300, 400);
+      context.stroke();
+      context.closePath();
+      // Draw east wall
+      context.beginPath();
+      context.moveTo(600, 300);
+      context.lineTo(400, 300);
+      context.stroke();
+      context.closePath();
     }
   }, []);
 
