c93d7c4b3491174002d40333ace31d2e8bc845a5
xmliszt <xmliszt@gmail.com>
2021-09-04 00:30:10 +0800

    feat: add color to each room

--- costume-party/src/components/Canvas.tsx
+++ costume-party/src/components/Canvas.tsx
@@ -5,18 +5,12 @@ export default function Canvas(): React.ReactElement {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
     const context = canvasRef.current?.getContext("2d");
     if (context) {
       context.strokeStyle = "#515151";
       context.lineWidth = 10;
       // Boundary wall
-      context.strokeRect(
-        0,
-        0,
-        canvas?.clientWidth || 600,
-        canvas?.clientHeight || 600
-      );
+      context.strokeRect(0, 0, 600, 600);
       // Center room wall
       context.strokeRect(200, 200, 200, 200);
       // Draw north wall
@@ -42,6 +36,54 @@ export default function Canvas(): React.ReactElement {
       context.moveTo(600, 300);
       context.lineTo(400, 300);
       context.stroke();
+      context.closePath();
+
+      // Color NW room
+      context.beginPath();
+      context.moveTo(0, 0);
+      context.lineTo(300, 0);
+      context.lineTo(300, 200);
+      context.lineTo(200, 200);
+      context.lineTo(200, 300);
+      context.lineTo(0, 300);
+      context.fillStyle = "rgba(249, 169, 162, 0.5)";
+      context.fill();
+      context.closePath();
+
+      // Color SW room
+      context.beginPath();
+      context.moveTo(0, 600);
+      context.lineTo(300, 600);
+      context.lineTo(300, 400);
+      context.lineTo(200, 400);
+      context.lineTo(200, 300);
+      context.lineTo(0, 300);
+      context.fillStyle = "rgba(169, 249, 162, 0.5)";
+      context.fill();
+      context.closePath();
+
+      // Color SE room
+      context.beginPath();
+      context.moveTo(600, 600);
+      context.lineTo(300, 600);
+      context.lineTo(300, 400);
+      context.lineTo(400, 400);
+      context.lineTo(400, 300);
+      context.lineTo(600, 300);
+      context.fillStyle = "rgba(249, 245, 162, 0.5)";
+      context.fill();
+      context.closePath();
+
+      // Color NE room
+      context.beginPath();
+      context.moveTo(600, 0);
+      context.lineTo(300, 0);
+      context.lineTo(300, 200);
+      context.lineTo(400, 200);
+      context.lineTo(400, 300);
+      context.lineTo(600, 300);
+      context.fillStyle = "rgba(116, 177, 247, 0.5)";
+      context.fill();
       context.closePath();
     }
   }, []);
