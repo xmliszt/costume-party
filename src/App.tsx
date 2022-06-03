d5b767a5825c0f0985adc1184b9024da953be231
Li Yuxuan <44829092+xmliszt@users.noreply.github.com>
2022-06-03 20:07:10 +0800

    feat: added BGM

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,39 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Playground from "./views/Playground";
 import Home from "./views/Home";
 import "./App.css";
+import Sound from "react-sound";
+import { useEffect, useState } from "react";
+import { Switch as AntSwitch, Typography } from "antd";
+import { SoundOutlined } from "@ant-design/icons";
 
 function App(): React.ReactElement {
+  const [playStatus, setPlayStatus] = useState<
+    "PLAYING" | "STOPPED" | "PAUSED"
+  >("PLAYING");
+
+  const toggleSound = (checked: boolean) => {
+    if (checked) {
+      setPlayStatus("PLAYING");
+    } else {
+      setPlayStatus("PAUSED");
+    }
+  };
+
+  useEffect(() => {
+    toggleSound(false);
+    toggleSound(true);
+  }, []);
+
   return (
     <div className="App">
+      <div className="music-control">
+        <Typography.Text style={{ color: "white" }}>BGM: </Typography.Text>
+        <AntSwitch
+          checkedChildren={<SoundOutlined />}
+          defaultChecked
+          onChange={toggleSound}
+        />
+      </div>
       <Router>
         <Switch>
           <Route exact path="/">
@@ -16,6 +45,11 @@ function App(): React.ReactElement {
           </Route>
         </Switch>
       </Router>
+      <Sound
+        url={`${process.env.PUBLIC_URL}/bg.mp3`}
+        playStatus={playStatus}
+        playFromPosition={300}
+      />
     </div>
   );
 }
